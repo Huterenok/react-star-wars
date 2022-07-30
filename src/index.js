03403ed70c7c14125c4c1ffbd1daf03de61cbb75
Huterok228 <clash.of.clans23916@gmail.com>
2022-07-30 22:33:27 +0300

    add deploy scripts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { REPO_NAME } from '@constants/repo';
 import store from '@store/store';
 
 import ThemeProvider from '@context/ThemeProvider';
@@ -12,7 +12,7 @@ import '@styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
+	<BrowserRouter basename={`/${REPO_NAME}/`}>
   	<React.StrictMode>
 			<Provider store={store}>
 				<ThemeProvider>
